433edaf3bfd7a40a976998859744bb01c78a5e46
jonagull <jonathangullik@gmail.com>
2023-04-18 08:45:52 +0200

    building routing, setting up file structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Hamburger } from "svelte-hamburgers";
+    import Menu from "../Menu.svelte";
+
+    let open: boolean;
+</script>
+
+<Hamburger bind:open --color="white" />
+
+<Menu bind:open />
+
+<h1>welcojme to karate</h1>
