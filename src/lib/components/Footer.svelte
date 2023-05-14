7e88ac5148e5ab83b35f7c868919698c9405caeb
Boien <59290906+jonagull@users.noreply.github.com>
2023-05-14 14:02:07 +0200

    changed phone number

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,32 +1,32 @@
 <footer
-    class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6"
+  class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6"
 >
-    <span class="text-sm text-gray-500 sm:text-center"
-        >© 2023 <a
-            target="_blank"
-            href="https://jonathangulliksen.no/"
-            class="hover:underline"
-            rel="noreferrer">Gulliksen Media™</a
-        >
-    </span>
-    <ul
-        class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0"
+  <span class="text-sm text-gray-500 sm:text-center"
+    >© 2023 <a
+      target="_blank"
+      href="https://jonathangulliksen.no/"
+      class="hover:underline"
+      rel="noreferrer">Gulliksen Media™</a
     >
-        <li>
-            <a
-                class="mr-4 hover:underline md:mr-6"
-                target="_blank"
-                href="https://medlemskap.nif.no/Start/Index/427950"
-                rel="noreferrer">Bli med!</a
-            >
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">94855321</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6"
-                >Havnegata 9, 4250 Kopervik</a
-            >
-        </li>
-    </ul>
+  </span>
+  <ul
+    class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0"
+  >
+    <li>
+      <a
+        class="mr-4 hover:underline md:mr-6"
+        target="_blank"
+        href="https://medlemskap.nif.no/Start/Index/427950"
+        rel="noreferrer">Bli med!</a
+      >
+    </li>
+    <li>
+      <a href="#" class="mr-4 hover:underline md:mr-6">928 37 931</a>
+    </li>
+    <li>
+      <a href="#" class="mr-4 hover:underline md:mr-6"
+        >Havnegata 9, 4250 Kopervik</a
+      >
+    </li>
+  </ul>
 </footer>
