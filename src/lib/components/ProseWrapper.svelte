c29d49097a465d81869affc132dbe78166e17e86
Boien <59290906+jonagull@users.noreply.github.com>
2023-05-18 14:58:30 +0200

    created ProseWrapper

---
+++ src/lib/components/ProseWrapper.svelte
@@ -0,0 +1,22 @@
+<div class="container mx-auto px-4 sm:px-6 lg:px-8">
+  <div
+    class="prose sm:prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl mx-auto"
+  >
+    <slot />
+  </div>
+</div>
+
+<!-- <article
+      class={data.slug === PathNames.Boardmembers ||
+      data.slug === PathNames.Training ||
+      data.slug === PathNames.History
+        ? "prose min-w-400"
+        : "prose "}
+    >
+      {#if data}
+        <div>
+          <h1>{data.title}</h1>
+          {@html data.content}
+        </div>
+      {/if}
+    </article> -->
