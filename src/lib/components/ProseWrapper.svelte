eda2bbed08abda5e782a41fa33477de61b3f0c39
jonagull <jonathangullik@gmail.com>
2023-05-18 17:43:40 +0200

    css

--- src/lib/components/ProseWrapper.svelte
+++ src/lib/components/ProseWrapper.svelte
@@ -1,7 +1,7 @@
 <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-  <div
-    class="prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl mx-auto table-responsive"
-  >
-    <slot />
-  </div>
+    <div
+        class="prose mb-20 sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl mx-auto table-responsive"
+    >
+        <slot />
+    </div>
 </div>
