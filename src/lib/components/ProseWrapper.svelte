62123e0c02003f10f1523536e0f07911d6c139fa
Boien <59290906+jonagull@users.noreply.github.com>
2023-05-18 16:42:44 +0200

    more responsive

--- src/lib/components/ProseWrapper.svelte
+++ src/lib/components/ProseWrapper.svelte
@@ -1,22 +1,7 @@
 <div class="container mx-auto px-4 sm:px-6 lg:px-8">
   <div
-    class="prose sm:prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl mx-auto"
+    class="prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl mx-auto table-responsive"
   >
     <slot />
   </div>
 </div>
-
-<!-- <article
-      class={data.slug === PathNames.Boardmembers ||
-      data.slug === PathNames.Training ||
-      data.slug === PathNames.History
-        ? "prose min-w-400"
-        : "prose "}
-    >
-      {#if data}
-        <div>
-          <h1>{data.title}</h1>
-          {@html data.content}
-        </div>
-      {/if}
-    </article> -->
