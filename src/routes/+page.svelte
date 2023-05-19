4716c23b110112cdaccaf06234c65f196c5c7a46
Boien <59290906+jonagull@users.noreply.github.com>
2023-05-19 20:01:10 +0200

    formattgin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,36 @@
 <script lang="ts">
-    import Hero from "$lib/components/Hero.svelte";
-    import { onMount } from "svelte";
-    import type { PageData } from "./$types";
+  import Hero from "$lib/components/Hero.svelte";
+  import { onMount } from "svelte";
+  import type { PageData } from "./$types";
 
-    export let data: PageData;
-    let hasLatestPosts = false;
-    let latestPostLabel: string;
+  export let data: PageData;
+  let hasLatestPosts = false;
+  let latestPostLabel: string;
 
-    onMount(() => {
-        if (data.latestsPosts) {
-            if (data.latestsPosts.length) {
-                hasLatestPosts = true;
+  onMount(() => {
+    if (data.latestsPosts) {
+      if (data.latestsPosts.length) {
+        hasLatestPosts = true;
 
-                latestPostLabel =
-                    data.latestsPosts.length > 1
-                        ? "Siste nyheter"
-                        : "Siste nyhet";
-            }
-        }
-    });
+        latestPostLabel =
+          data.latestsPosts.length > 1 ? "Siste nyheter" : "Siste nyhet";
+      }
+    }
+  });
 </script>
 
 <Hero {hasLatestPosts} {latestPostLabel} />
 
 {#if hasLatestPosts}
-    <article class="prose">
-        <h1 id="siste">{latestPostLabel}</h1>
+  <article class="prose">
+    <h1 id="siste">{latestPostLabel}</h1>
 
-        {#each data.latestsPosts as p}
-            <div>
-                <h2>{p.attributes.title}</h2>
-                {@html p.attributes.text}
-                <hr />
-            </div>
-        {/each}
-    </article>
+    {#each data.latestsPosts as p}
+      <div>
+        <h2>{p.attributes.title}</h2>
+        {@html p.attributes.text}
+        <hr />
+      </div>
+    {/each}
+  </article>
 {/if}
