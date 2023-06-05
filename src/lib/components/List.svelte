883336f43f8a07096b50ca080a1ab1efd13a875f
jonagull <jonathangullik@gmail.com>
2023-06-05 21:58:09 +0200

    init image modal carousel

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -1,112 +1,118 @@
 <script lang="ts">
-  import { AUTH_TOKEN, BASE_URL } from "$lib/constants/variables";
-  import { formatDate } from "$lib/helpers/helpers";
-  import { onMount } from "svelte";
-  import {
-    Table,
-    TableBody,
-    TableBodyCell,
-    TableBodyRow,
-    TableHead,
-    TableHeadCell,
-  } from "flowbite-svelte";
-  import { fetchPageData } from "$lib/helpers/api";
-  import { PathNames } from "$lib/constants/pathNames.enum";
+    import { AUTH_TOKEN, BASE_URL } from "$lib/constants/variables";
+    import { formatDate } from "$lib/helpers/helpers";
+    import { onMount } from "svelte";
+    import {
+        Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+    } from "flowbite-svelte";
+    import { fetchPageData } from "$lib/helpers/api";
+    import { PathNames } from "$lib/constants/pathNames.enum";
 
-  let listData: any;
-  export let slug: string;
+    let listData: any;
+    export let slug: string;
 
-  const listPaths: { [key: string]: string } = {
-    [PathNames.Galleries]: "gallery",
-    [PathNames.Posts]: "post",
-    [PathNames.Competitions]: "competition",
-  };
+    const listPaths: { [key: string]: string } = {
+        [PathNames.Galleries]: "gallery",
+        [PathNames.Posts]: "post",
+        [PathNames.Competitions]: "competition",
+    };
 
-  const paginationFetch = async (page: any) => {
-    if (page === 0 || page > listData.meta.pagination.pageCount) {
-      return;
-    }
+    const paginationFetch = async (page: any) => {
+        if (page === 0 || page > listData.meta.pagination.pageCount) {
+            return;
+        }
 
-    const res = await fetch(
-      `${BASE_URL}/api/${listPaths[slug]}?pagination[page]=${page}`,
-      {
-        headers: {
-          Authorization: `Bearer ${AUTH_TOKEN}`,
-        },
-      }
-    );
-    listData = await res.json();
-  };
+        const res = await fetch(
+            `${BASE_URL}/api/${listPaths[slug]}?pagination[page]=${page}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${AUTH_TOKEN}`,
+                },
+            }
+        );
+        listData = await res.json();
+    };
 
-  onMount(async () => {
-    listData = await fetchPageData(slug);
-  });
+    onMount(async () => {
+        listData = await fetchPageData(slug);
+    });
 </script>
 
 {#if listData && listData.data && listData.data.length > 0}
-  <div class="flex flex-col">
-    <Table hoverable={true}>
-      <TableHead>
-        <TableHeadCell>Handling</TableHeadCell>
-        <TableHeadCell>Tittel</TableHeadCell>
-        <TableHeadCell>Dato</TableHeadCell>
-      </TableHead>
-      <TableBody>
-        {#each listData.data as p}
-          <TableBodyRow>
-            <TableBodyCell>
-              <a
-                class="font-medium text-blue-600 hover:underline"
-                href={`/${listPaths[slug]}/${p.id}`}>Se</a
-              >
-            </TableBodyCell>
-            <TableBodyCell>{p.attributes.title}</TableBodyCell>
-            <TableBodyCell
-              >{formatDate(p.attributes.createdAt || "")}</TableBodyCell
-            >
-          </TableBodyRow>
-        {/each}
-      </TableBody>
-    </Table>
+    <div class="flex flex-col">
+        <Table hoverable={true}>
+            <TableHead>
+                <TableHeadCell>Handling</TableHeadCell>
+                <TableHeadCell>Tittel</TableHeadCell>
+                <TableHeadCell>Dato</TableHeadCell>
+            </TableHead>
+            <TableBody>
+                {#each listData.data as p}
+                    <TableBodyRow>
+                        <TableBodyCell>
+                            <a
+                                class="font-medium text-blue-600 hover:underline"
+                                href={`/${listPaths[slug]}/${p.id}`}>Se</a
+                            >
+                        </TableBodyCell>
+                        <TableBodyCell>{p.attributes.title}</TableBodyCell>
+                        <TableBodyCell
+                            >{formatDate(
+                                p.attributes.createdAt || ""
+                            )}</TableBodyCell
+                        >
+                    </TableBodyRow>
+                {/each}
+            </TableBody>
+        </Table>
 
-    {#if listData.meta.pagination.pageCount > 1}
-      <nav class="mt-5 mb-20 flex flex-col items-center">
-        <ul class="inline-flex -space-x-px">
-          <li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a
-              href="#"
-              on:click={() =>
-                paginationFetch(listData.meta.pagination.page - 1)}
-              class="pag">Tilbake</a
-            >
-          </li>
-          {#each Array(listData.meta.pagination.pageCount) as _, i}
-            <li>
-              <!-- svelte-ignore a11y-invalid-attribute -->
-              <a
-                href="#"
-                on:click={() => paginationFetch(i + 1)}
-                class={listData.meta.pagination.page === i + 1
-                  ? "current-pag"
-                  : "pag"}
-              >
-                {i + 1}
-              </a>
-            </li>
-          {/each}
+        {#if listData.meta.pagination.pageCount > 1}
+            <nav class="mt-5 mb-20 flex flex-col items-center">
+                <ul class="inline-flex -space-x-px">
+                    <li>
+                        <!-- svelte-ignore a11y-invalid-attribute -->
+                        <a
+                            href="#"
+                            on:click={() =>
+                                paginationFetch(
+                                    listData.meta.pagination.page - 1
+                                )}
+                            class="pag">Tilbake</a
+                        >
+                    </li>
+                    {#each Array(listData.meta.pagination.pageCount) as _, i}
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a
+                                href="#"
+                                on:click={() => paginationFetch(i + 1)}
+                                class={listData.meta.pagination.page === i + 1
+                                    ? "current-pag"
+                                    : "pag"}
+                            >
+                                {i + 1}
+                            </a>
+                        </li>
+                    {/each}
 
-          <li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <a
-              href="#"
-              class="pag"
-              on:click={() =>
-                paginationFetch(listData.meta.pagination.page + 1)}>Neste</a
-            >
-          </li>
-        </ul>
-      </nav>
-    {/if}
-  </div>
+                    <li>
+                        <!-- svelte-ignore a11y-invalid-attribute -->
+                        <a
+                            href="#"
+                            class="pag"
+                            on:click={() =>
+                                paginationFetch(
+                                    listData.meta.pagination.page + 1
+                                )}>Neste</a
+                        >
+                    </li>
+                </ul>
+            </nav>
+        {/if}
+    </div>
 {/if}
--- src/routes/gallery/[id]/+page.svelte
+++ src/routes/gallery/[id]/+page.svelte
@@ -2,8 +2,46 @@
     import ProseWrapper from "$lib/components/ProseWrapper.svelte";
     import { BASE_URL } from "$lib/constants/variables";
     import type { PageData } from "./$types";
+    import { Carousel } from "flowbite-svelte";
 
     export let data: PageData;
+
+    let open = false;
+    let selectedImageIndex: number;
+
+    let imageArray = data.gallery.attributes.pics.data.map((x) => ({
+        id: x.id,
+        name: "lol",
+        imageUrl: BASE_URL + x.attributes.url,
+        attribution: "mark-harpur-K2s_YE031CA-unsplash",
+    }));
+
+    console.log(imageArray);
+
+    const openImagePreviewer = (index: number): void => {
+        console.log(imageArray.length);
+        console.log(index);
+        selectedImageIndex = index + 1;
+        open = !open;
+    };
+
+    const nextImage = () => {
+        if (imageArray.length === selectedImageIndex) {
+            selectedImageIndex = 1;
+            return;
+        }
+
+        selectedImageIndex += 1;
+    };
+
+    const previousImage = () => {
+        if (imageArray.length === 0) {
+            selectedImageIndex = imageArray.length;
+            return;
+        }
+
+        selectedImageIndex -= 1;
+    };
 </script>
 
 {#if data}
@@ -16,13 +54,41 @@
 
     <div class="container grid grid-cols-3 gap-2 mx-auto">
         {#if data.gallery.attributes.pics.data}
-            {#each data.gallery.attributes.pics.data as pic}
+            {#each data.gallery.attributes.pics.data as pic, i}
                 <div class="w-full rounded">
-                    <a target="_blank" href={BASE_URL + pic.attributes.url}>
-                        <img src={BASE_URL + pic.attributes.url} alt="" />
-                    </a>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <img
+                        src={BASE_URL + pic.attributes.url}
+                        alt=""
+                        on:click={() => openImagePreviewer(i)}
+                    />
                 </div>
             {/each}
         {/if}
     </div>
+
+    <button on:click={() => (open = !open)}>open me</button>
+    <dialog data-modal {open}>
+        <div class="carousel-container">
+            <img
+                src={imageArray.find((x) => x.id === selectedImageIndex)
+                    ?.imageUrl}
+                alt=""
+            />
+            <button on:click={() => (open = !open)}>Close</button>
+            <button on:click={() => nextImage()}>Next</button>
+            <button on:click={() => previousImage()}>Previous</button>
+        </div>
+    </dialog>
 {/if}
+
+<style>
+    .carousel-container {
+        border: 1px solid black;
+        padding: 10px;
+        height: auto;
+        width: 60%;
+        margin-right: auto;
+        margin-left: auto;
+    }
+</style>
