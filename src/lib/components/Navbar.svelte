8dde646bfa8f331c369a4567703d2b6db76c9c76
jonagull <jonathangullik@gmail.com>
2023-06-20 20:52:46 +0200

    revert my silly doings

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -10,13 +10,6 @@
         Chevron,
     } from "flowbite-svelte";
     import KarateMan from "$lib/assets/karateBoi.png";
-
-    let isMobile = false;
-    let hidden = isMobile;
-
-    function toggle() {
-        hidden = !hidden;
-    }
 </script>
 
 <Navbar let:hidden let:toggle>
@@ -35,51 +28,37 @@
     <NavHamburger on:click={toggle} />
     <NavUl {hidden}>
         <NavLi href="/" active={true}>Fremsiden</NavLi>
-        <NavLi href="/training" on:click={toggle}>Treningstider</NavLi>
-        <NavLi href="/posts" on:click={toggle}>Nyheter</NavLi>
-        <NavLi href="/competitions" on:click={toggle}>Resultater</NavLi>
-        <NavLi href="/pics" on:click={toggle}>Bildearkiv</NavLi>
+        <NavLi href="/training">Treningstider</NavLi>
+        <NavLi href="/posts">Nyheter</NavLi>
+        <NavLi href="/competitions">Resultater</NavLi>
+        <NavLi href="/pics">Bildearkiv</NavLi>
 
-        <NavLi id="nav-menu1" class="cursor-pointer">
-            <Chevron aligned>Karate</Chevron>
-        </NavLi>
+        <NavLi id="nav-menu1" class="cursor-pointer"
+            ><Chevron aligned>Karate</Chevron></NavLi
+        >
         <Dropdown triggeredBy="#nav-menu1" class="w-44 z-20">
-            <DropdownItem href="/rating" on:click={toggle}
-                >Gradering</DropdownItem
-            >
-            <DropdownItem href="/kihon" on:click={toggle}
-                >Kihon/Kumite</DropdownItem
-            >
-            <DropdownItem href="/kata" on:click={toggle}>Kata</DropdownItem>
-            <DropdownItem href="/encyclopedia" on:click={toggle}
-                >Leksikon</DropdownItem
-            >
+            <DropdownItem href="/rating">Gradering</DropdownItem>
+            <DropdownItem href="/kihon">Kihon/Kumite</DropdownItem>
+            <DropdownItem href="/kata">Kata</DropdownItem>
+            <DropdownItem href="/encyclopedia">Leksikon</DropdownItem>
         </Dropdown>
 
-        <NavLi id="nav-menu2" class="cursor-pointer" on:click={toggle}>
-            <Chevron aligned>Klubben</Chevron>
-        </NavLi>
+        <NavLi id="nav-menu2" class="cursor-pointer"
+            ><Chevron aligned>Klubben</Chevron></NavLi
+        >
         <Dropdown triggeredBy="#nav-menu2" class="w-44 z-20">
-            <DropdownItem href="/blackbelts" on:click={toggle}
-                >Sortbelter</DropdownItem
-            >
-            <DropdownItem href="/trainers" on:click={toggle}
-                >Instruktører</DropdownItem
-            >
-            <DropdownItem href="/boardmember" on:click={toggle}
-                >Styret</DropdownItem
-            >
+            <DropdownItem href="/blackbelts">Sortbelter</DropdownItem>
+            <DropdownItem href="/trainers">Instruktører</DropdownItem>
+            <DropdownItem href="/boardmember">Styret</DropdownItem>
         </Dropdown>
 
-        <NavLi id="nav-menu3" class="cursor-pointer" on:click={toggle}>
-            <Chevron aligned>Diverse</Chevron>
-        </NavLi>
+        <NavLi id="nav-menu3" class="cursor-pointer"
+            ><Chevron aligned>Diverse</Chevron></NavLi
+        >
         <Dropdown triggeredBy="#nav-menu3" class="w-44 z-20">
-            <DropdownItem href="/links" on:click={toggle}>Linker</DropdownItem>
+            <DropdownItem href="/links">Linker</DropdownItem>
             <!-- <DropdownItem href="/galleries">Bildegalleriet</DropdownItem> -->
-            <DropdownItem href="/history" on:click={toggle}
-                >Historie</DropdownItem
-            >
+            <DropdownItem href="/history">Historie</DropdownItem>
         </Dropdown>
     </NavUl>
 </Navbar>
